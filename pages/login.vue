b7b45a014886b298779d20df1c9015735c333c2c
Emmanuelle Zenou <emmanuelle.zenou.rogala@gmail.com>
2021-04-27 16:15:52 +0200

    first config

---
+++ pages/login.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-form>
+      <h1>Login</h1>
+      <v-text-field v-model="email" label="Mail"></v-text-field>
+      <v-text-field v-model="password" label="Mot de passe"></v-text-field>
+      <v-btn @click="submit"></v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase/app";
+import "firebase/auth";
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    submit() {
+      // [START auth_signin_password]
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then((userCredential) => {
+          // Signed in
+          let user = userCredential.user;
+          console.log (user)
+          // ...
+        })
+        .catch((error) => {
+          let errorCode = error.code;
+          let errorMessage = error.message;
+          console.log (error)
+
+        });
+      // [END auth_signin_password]
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ plugins/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase'
+import 'firebase/auth'
+
+const config = {
+  apiKey: "AIzaSyCTk3EG6jR9oN8MgeaSkeGIiEoPoRSp144",
+  authDomain: "sportbase-38151.firebaseapp.com",
+  projectId: "sportbase-38151",
+  storageBucket: "sportbase-38151.appspot.com",
+  messagingSenderId: "329953458846",
+  appId: "1:329953458846:web:98fbb33715430d1aa4998f",
+  measurementId: "G-XN6ZS185C2"
+}
+
+!firebase.apps.length ? firebase.initializeApp(config) : '';
+
+
+export const auth = firebase.auth();
+export const github = new firebase.auth.GithubAuthProvider();
+export default firebase;
